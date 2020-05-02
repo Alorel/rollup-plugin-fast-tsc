fbcbb407ca04ee3b6517e0951c38be911fefea79
Arturas Molcanovas <a.molcanovas@gmail.com>
2020-05-02 17:25:17 +0100

    fix: Fixed weird behaviours, made the plugin into a simple spawn wrapper
    BREAKING CHANGE: Plugin config changed, see updated typings

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,17 @@
 import * as spawn from 'cross-spawn';
 import {join, relative} from 'path';
 import {Plugin, PluginContext} from 'rollup';
-import {init} from './lib/init';
-import {Loader, resolveLoader} from './lib/loader';
+import {sourceMapLoader} from './lib/sourceMapLoader';
 import {spawnPromise} from './lib/spawnPromise';
+import {tmpDir} from './lib/tmpDir';
 
 const extRe = /\.tsx?$/;
 
 interface Opts {
+  baseDir?: string;
+
   /** Additional CLI args to pass to tsc */
   extraCliArgs?: string[];
-
-  /**
-   * Project dir
-   * @default process.cwd()
-   */
-  mainPath?: string;
-
-  /**
-   * Tsconfig path
-   * @default tsconfig.json
-   */
-  tsconfig?: string;
 
   /** Whether we're operating in watch mode */
   watch?: boolean;
@@ -29,15 +19,12 @@ interface Opts {
 
 function plugin(opts: Opts = {}): Plugin {
   const {
+    baseDir = process.cwd(),
     extraCliArgs = [],
-    mainPath = process.cwd(),
-    tsconfig,
     watch = false
   } = opts;
 
   let tmpDirPath: string;
-  // let cfg: ts.ParsedCommandLine;
-  let loaderFn: Loader;
 
   const args: string[] = [
     require.resolve('typescript/bin/tsc'),
@@ -45,45 +32,48 @@ function plugin(opts: Opts = {}): Plugin {
     '--outDir'
   ];
 
-  let done: Promise<void> = init(mainPath, tsconfig)
-    .then(({dir, config}) => {
-      loaderFn = resolveLoader(extraCliArgs.includes('--sourceMap') || config.options.sourceMap);
-      args.push(tmpDirPath = dir);
+  let initialised = false;
 
-      return spawnPromise(process.execPath, args);
-    });
+  function initialise(): Promise<void> {
+    initialised = true;
 
-  if (watch) {
-    // false positive - caught below
-    done = done //tslint:disable-line:no-floating-promises
-      .then(() => {
-        // Don't return
-        spawn(process.execPath, args.concat('--watch', '--preserveWatchOutput'));
+    let done: Promise<void> = tmpDir()
+      .then(dir => {
+        args.push(tmpDirPath = dir);
+
+        return spawnPromise(process.execPath, args);
       });
-  }
 
-  done
-    .catch(() => {
-      // noop - will be handled in buildStart
-    });
+    if (watch) {
+      // false positive - caught below
+      done = done //tslint:disable-line:no-floating-promises
+        .then(() => {
+          // Don't return
+          spawn(process.execPath, args.concat('--watch', '--preserveWatchOutput'));
+        });
+    }
+
+    return done;
+  }
 
   return {
     buildStart() {
-      return done;
+      if (!initialised) {
+        return initialise();
+      }
     },
     load(this: PluginContext, id) {
       if (!extRe.test(id)) {
         return null;
       }
 
-      const newId = join(tmpDirPath, relative(process.cwd(), id))
+      const newId = join(tmpDirPath, relative(baseDir, id))
         .replace(extRe, '.js');
       watch && this.addWatchFile(newId);
 
-      return loaderFn(newId);
+      return sourceMapLoader(newId);
     },
     name: 'fast-tsc'
-
   };
 }
 
--- src/lib/sourceMapLoader.ts
+++ src/lib/sourceMapLoader.ts
@@ -13,23 +13,12 @@ function onBothRead([code, map]: [string, SourceMapInput]): LoadResult {
   return {code, map};
 }
 
-function sourceMapLoader(id: string): Promise<LoadResult> {
+/** @internal */
+export function sourceMapLoader(id: string): Promise<LoadResult> {
   const code$ = fs.readFile(id, 'utf8');
   const map$ = fs.readFile(`${id}.map`, 'utf8')
     .then(onSrcMapRead, onSrcMapError);
 
   return Promise.all([code$, map$])
     .then(onBothRead);
-}
-
-function regularLoader(id): Promise<LoadResult> {
-  return fs.readFile(id, 'utf8');
-}
-
-/** @internal */
-export type Loader = (id: string) => Promise<LoadResult>;
-
-/** @internal */
-export function resolveLoader(sourceMap?: boolean): Loader { //tslint:disable-line:bool-param-default
-  return sourceMap ? sourceMapLoader : regularLoader;
 }
